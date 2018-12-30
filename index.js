7dff1625d89e466b53492789bc1f6daff9fbb78b
BEAU WITKA <bwitka@gmail.com>
2018-12-30 21:01:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+//returns the first element that matches the selector:
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+//pulls a .target out of #nested:
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+//returns the most deeply nested child in #grand-node:
+function deepestChild() {
+  let stringOfDivs = document.querySelectorAll('#grand-node div');
+  return stringOfDivs[stringOfDivs.length - 1];
+}
+
+//increases ranks in .ranked-list by n:
+function  increaseRankBy(n) {
+  const ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
