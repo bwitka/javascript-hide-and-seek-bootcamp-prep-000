485fe16ec023abb68342ac77e267377513e96a90
BEAU WITKA <bwitka@gmail.com>
2018-12-30 20:34:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nestedTarget() {
 
 //returns the most deeply nested child in #grand-node:
 function deepestChild() {
-  return document.getElementById('grand-node');
+  return document.getElementById('grand-node').querySelector('div div div div');
 }
 
 //increases ranks in .ranked-list by n:
